11d55abcf87ebea514a70c3030d65757d77082ab
pfarnach <tefl.pat@gmail.com>
2015-02-17 10:53:48 -0800

    Basic functionality completed. Started adding in search method using Google API ajax request

--- js/main.js
+++ js/main.js
@@ -1,4 +1,45 @@
+var latitude, longitude;
+
 $(document).ready(function(){
+
+	function handle(e) {
+		if (e.keyCode === 13) {
+			alert("Enter was pressed");
+		}
+	}
+
+	function get_coordinates(location_name) {
+
+		$.ajax({
+
+			// The 'type' property sets the HTTP method.
+			// A value of 'PUT' or 'DELETE' will trigger a preflight request.
+			type: 'GET',
+
+			// google map API doesn't support JSONP format
+			dataType: 'json',
+
+			// The URL to make the request to.
+			// url: "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBsQg7uGxTdAUNaGVw4qWQ1wiYkfoPx6HQ",
+			url: "https://maps.googleapis.com/maps/api/geocode/json?address=Madrid,+Spain&key=AIzaSyBsQg7uGxTdAUNaGVw4qWQ1wiYkfoPx6HQ",
+
+			success: function(data) {
+				latitude = data['results'][0]['geometry']['location']['lat'];
+				longitude = data['results'][0]['geometry']['location']['lng'];
+				console.log(data);
+				// format_location(data);
+			},
+
+			error: function() {
+				console.log("AJAX request for Geocode API failed.");
+			},
+
+			async: false,
+		});
+
+	}
+	
+	console.log(latitude + " " + longitude + "!");
 
 	$.ajax({
 
@@ -9,18 +50,19 @@ $(document).ready(function(){
 		dataType: 'jsonp',
 
 		// The URL to make the request to.
-		url: "https://api.forecast.io/forecast/e27ae1f9f8be2c73c1648f94f81cf04a/45.513334,-122.653049",
-		// url: "https://api.forecast.io/forecast/e27ae1f9f8be2c73c1648f94f81cf04a/43.0469,-76.1444",
+		// url: "https://api.forecast.io/forecast/e27ae1f9f8be2c73c1648f94f81cf04a/" + latitude + "," + longitude,
+		url: "https://api.forecast.io/forecast/e27ae1f9f8be2c73c1648f94f81cf04a/43.0469,-76.1444",
 		// url: "https://api.forecast.io/forecast/e27ae1f9f8be2c73c1648f94f81cf04a/40.4,-3.6833",
 
 		success: function(data) {
-		// Here's where you handle a successful response.
+			// Here's where you handle a successful response.
 			format_data(data);
 		},
 
 		error: function() {
-			console.log("AJAX request failed.");
-		}
+			console.log("AJAX request for Weather IO API failed.");
+		},
+
 	});
 
 	function format_data(data) {
@@ -48,6 +90,10 @@ $(document).ready(function(){
 		// icon
 		var current_icon = data['currently']['icon'];
 		add_icon_class(current_icon, ".current-icon");
+
+		// precip chance
+		var current_precip = data['currently']['precipProbability'] * 100;
+		$('div[data-city="Portland, OR"] .current-precip').html(current_precip);
 
 
 		// ** ALERTS **
@@ -102,49 +148,69 @@ $(document).ready(function(){
 		var later_humidity = Math.round(data['hourly']['data'][6]['humidity'] * 100);
 		$('div[data-city="Portland, OR"] .later-humidity').html(later_humidity);
 
+		// precip chance
+		var later_precip = data['hourly']['data'][6]['precipProbability'] * 100;
+		$('div[data-city="Portland, OR"] .later-precip').html(later_precip);
+
+
 		// ** HOURLY **
-		for (var i=1; i < 3; i++) {
+		for (var i=0; i < 12; i++) {
 			var time = new Date(data['hourly']['data'][i]['time'] * 1000),
 				temp = Math.round(data['hourly']['data'][i]['temperature']),
 				summary = data['hourly']['data'][i]['summary'],
 				icon = data['hourly']['data'][i]['icon'],
 				precip = Math.round(data['hourly']['data'][i]['precipProbability'] * 100);
 
-			var hour = time.getHours();
-			var hour_suffix = hour >= 12 ? "pm" : "am";
-			hour = ((hour + 11) % 12);
-			console.log(hour + hour_suffix);
-
-			console.log(icon);
+			var hour = time.toLocaleTimeString(navigator.language, {timeZone: data['timezone'], hour: 'numeric'});
 
 			// time
-			$('div[data-city="Portland, OR"] .hour' + [i] +'-time').html(hour + hour_suffix);
+			$('div[data-city="Portland, OR"] .hour' + [i+1] +'-time').html(hour);
 
 			// temp
-			$('div[data-city="Portland, OR"] .hour' + [i] +'-temp').html(" " + temp);
+			$('div[data-city="Portland, OR"] .hour' + [i+1] +'-temp').html(" " + temp);
 
 			// summary
-			$('div[data-city="Portland, OR"] .hour' + [i] +'-summary').html(summary);
+			$('div[data-city="Portland, OR"] .hour' + [i+1] +'-summary').html(summary);
 
 			// icon
-			add_icon_class(icon, '.hour' + [i] +'-icon1');
+			add_icon_class(icon, '.hour' + [i+1] +'-icon');
 
 			// precip
-			$('div[data-city="Portland, OR"] .hour' + [i] +'-precip').html(" " + precip);
+			$('div[data-city="Portland, OR"] .hour' + [i+1] +'-precip').html(" " + precip);
 		}
 
 
+		// ** FORECAST ** 
 
-		// info about converting UNIX time: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-		var date = new Date(unix_time*1000);
-		var date_display = date.getDate();
-		var day = date.getDay();
-		var month = date.getMonth();
-		var year = date.getFullYear();
-		var hours = date.getHours();
-		var minutes = date.getMinutes();
-		var seconds = date.getSeconds();
+		for (var i = 1; i < 7; i++) {
+			var time = new Date(data['daily']['data'][i]['time'] * 1000),
+				temp_min = Math.round(data['daily']['data'][i]['temperatureMin']),
+				temp_max = Math.round(data['daily']['data'][i]['temperatureMax']),
+				summary = (data['daily']['data'][i]['summary']).slice(0,-1),
+				precip = Math.round(data['daily']['data'][i]['precipProbability'] * 100),
+				icon = data['daily']['data'][i]['icon'];
+
+			var day = (time.toLocaleTimeString(navigator.language, {timeZone: data['timezone'], weekday:'long'}).slice(0,-12));
+
+			// temp
+			$('div[data-city="Portland, OR"] .forecast' + [i] +'-temp-max').html(" " + temp_max);
+			$('div[data-city="Portland, OR"] .forecast' + [i] +'-temp-min').html(" " + temp_min);
+
+			// summary
+			$('div[data-city="Portland, OR"] .forecast' + [i] +'-summary').html(" " + summary);
+
+			// icon
+			add_icon_class(icon, '.forecast' + [i] +'-icon');
+
+			// weekday
+			$('div[data-city="Portland, OR"] .forecast' + [i] +'-day').html(" " + day);
+
+			// precip
+			$('div[data-city="Portland, OR"] .forecast' + [i] +'-precip').html(" " + precip);	
+		}
+
 	}
+
 
 	function add_icon_class(weather, destination_class) {
 		var icon_class;
@@ -154,7 +220,7 @@ $(document).ready(function(){
 				icon_class = "wi-day-sunny";
 				break;
 			case 'clear-night':
-				icon_class = "wi-stars";
+				icon_class = "wi-night-clear";
 				break;
 			case 'rain':
 				icon_class = "wi-rain";
@@ -187,11 +253,26 @@ $(document).ready(function(){
 		$('div[data-city="Portland, OR"] ' + destination_class).addClass('wi ' + icon_class);
 	}
 
+
 	function post_alert(title, description) {
 		$('div[data-city="Portland, OR"] .weather-alert').show();
 		$('div[data-city="Portland, OR"] .alert-text').html(title);
 	}
 
+	// delays displaying the whole screen to give ajax request some breathing room
 	$('div[data-city="Portland, OR"]').delay(800).fadeIn(800);
 
+	// minimize buttons
+	$('button.minimize').on('click', function(){
+		// traverses out of button, to parent, then to next (panel header --> panel body), then toggles it
+		$(this).parent().next().slideToggle(500);
+
+		// changes button text depending on what it currently is
+		$(this).html($(this).html() === "Expand" ? "Minimize" : "Expand");
+	});
+
 });
+
+
+
+
